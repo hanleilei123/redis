4f1d1ed3b01996ea314aba27b6510bf92b4ae3ce
hll <hanll@i2finance.net>
2020-03-06 18:09:11 +0800

    commit

--- src/main/java/com/example/demo/testGit.java
+++ src/main/java/com/example/demo/testGit.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
+import org.junit.Test;
+
 public class testGit {
 
     public void test1(){
 
     }
 
-
+    @Test
     public void test2(){
-
+        System.out.println("test2老子天下第一");
     }
 
 
